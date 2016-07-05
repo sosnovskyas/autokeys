a70b54fff55ad3aff3789e28bc7fad1430963298
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-07-05 19:12:29 +0300

    minimal config done

---
+++ webpack.config.babel.js
@@ -0,0 +1,19 @@
+'use strict';
+import webpack from 'webpack'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+
+export default {
+  entry: {
+    index: './src/index.js'
+  },
+  output: {
+    path: __dirname + "/dist",
+    filename: "[name].bundle.js",
+    chunkFilename: "[id].bundle.js"
+  },
+  plugins: [new HtmlWebpackPlugin()],
+  loaders: [
+    {test: /\.js$/, loader: 'babel'},
+    {test: /\.jade$/, loader: 'jade'}
+  ]
+}
