ce3283d14856c222080b62415a8e3d5f9608e867
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-07-06 01:47:29 +0300

    JS part ready

--- src/components/selector/index.js
+++ src/components/selector/index.js
@@ -1,4 +1,6 @@
 'use strict';
+import selectTemplate from './select.jade'
+import listTemplate from './list.jade'
 
 export default class Selector {
   constructor({
@@ -8,28 +10,82 @@ export default class Selector {
     year,
     list
   }) {
-    console.log(dataUrl,
-      brand,
-      model,
-      year,
-      list);
+    this.brand = brand;
+    this.model = model;
+    this.year = year;
+    this.list = list;
+
     this._loadData(dataUrl, data => {
-      this.data = data
+      this.data = data;
+      let brandList = [];
+
+      for (let item in this.data.cars) {
+        brandList.push(item);
+      }
+
+      this._updateSelect(brand, brandList, 'Выберите марку');
+      brand.addEventListener('change', event => this._onBrandChange(event.target.value));
+      model.addEventListener('change', event => this._onModelChange(event.target.value));
+      year.addEventListener('change', event => this._onYearChange(event.target.value));
     });
   }
 
   _loadData(url, callback) {
     fetch(url)
       .then(response=> {
-        if(response.status == 200) {
-          response.json()
+        if (response.status == 200) {
+          return response.json()
         } else {
-          throw new ReadError('ошибка получения данных')
+          throw new Error('ошибка получения данных')
         }
       })
       .then(data=> callback(data));
   }
 
+  _updateSelect(elem, data, defaultValue) {
+    elem.innerHTML = selectTemplate({
+      items: data,
+      defaultValue: defaultValue
+    });
+  }
+
+  _updateList(items) {
+    console.log('_updateList', items);
+    this.list.innerHTML = listTemplate({
+      items: items
+    })
+  }
+
+  _onBrandChange(item) {
+    console.log('_onBrandChange', item);
+    this._updateSelect(this.year, [], 'выберите год');
+    let modelList = [];
+
+    for (let model in this.data.cars[item]) {
+      modelList.push(model);
+    }
+    this._updateSelect(this.model, modelList, 'выберите модель');
+  }
+
+  _onModelChange(item) {
+    console.log('_onModleChange', item);
+    let yearList = [];
+    for (let year in this.data.cars[this.brand.value][item]) {
+      yearList.push(year);
+    }
+    this._updateSelect(this.year, yearList, 'выберите год');
+  }
+
+  _onYearChange(item) {
+    console.log('_onYearChange', item);
+
+    const keyList = this.data.cars[this.brand.value][this.model.value][this.year.value];
+    let keyItems = [];
+
+    keyList.map(item => keyItems.push(this.data.keys[item]));
+
+    this._updateList(keyItems);
+  }
 
 }
 
