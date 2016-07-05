bf8a6640050a4fc00d1e4a30784d87ede5bf3b9e
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-07-06 00:48:09 +0300

    added new component - Slector

--- src/components/selector/index.js
+++ src/components/selector/index.js
@@ -7,8 +7,29 @@ export default class Selector {
     model,
     year,
     list
-  }){
-
+  }) {
+    console.log(dataUrl,
+      brand,
+      model,
+      year,
+      list);
+    this._loadData(dataUrl, data => {
+      this.data = data
+    });
   }
+
+  _loadData(url, callback) {
+    fetch(url)
+      .then(response=> {
+        if(response.status == 200) {
+          response.json()
+        } else {
+          throw new ReadError('ошибка получения данных')
+        }
+      })
+      .then(data=> callback(data));
+  }
+
+
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 'use strict';
 import './style.styl'
 import Selector from './components/selector'
-new Selector();
+new Selector({
+  dataUrl: '/data.json',
+  brand: document.querySelector('.car-brand'),
+  model: document.querySelector('.car-model'),
+  year: document.querySelector('.car-year'),
+  list: document.querySelector('.key-list')
+});
 console.log('done');
