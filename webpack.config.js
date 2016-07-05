7da5e3e9cb413648b16888f4e9bc8888260e19c7
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-07-05 20:37:55 +0300

    config fixed

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+
+const pluginsProd = [
+  new ExtractTextPlugin('style.css', {allChunks: true}),
+  new HtmlWebpackPlugin({template: 'jade!./src/index.jade'}),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    }
+  })
+];
+
+const pluginsDev = [
+  new HtmlWebpackPlugin({template: './src/index.jade'}),
+  new webpack.HotModuleReplacementPlugin(),
+  new ExtractTextPlugin('style.css', {allChunks: true})
+];
+
+const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
+
+module.exports = {
+  watch: isDevelopment,
+  devtool: isDevelopment ? 'source-map' : null,
+  entry: [
+    './src/index.js'
+  ],
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'index.js',
+    publicPath: '/'
+  },
+
+  plugins: isDevelopment ? pluginsDev : pluginsProd,
+  module: {
+    loaders: [
+      {test: /\.jsx?$/, exclude: /node_modules/, loaders: ['babel?presets[]=es2015']},
+      {test: /\.jade/, exclude: /node_modules/, loader: 'jade-loader'},
+      {test: /\.styl/, loader: ExtractTextPlugin.extract(['css', 'postcss', 'stylus?sourceMap'])}
+    ]
+  },
+
+  postcss: [autoprefixer({browsers: ['ie >= 10', 'opera >= 12']})]
+};
